551d3831ee8349921da00ca3dd2723fd6ceeb56f
dkirkwoo <dkirkwoo@cisco.com>
2017-11-24 17:07:40 +1100

    first commit

---
+++ singleProbe.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+temp_sensor = '/sys/bus/w1/devices/28-031771c6acff/w1_slave'
+
+def temp_raw():
+	f = open(temp_sensor, 'r')
+	lines = f.readlines()
+	f.close
+	return lines
+
+def read_temp():
+	lines = temp_raw()
+	while lines[0].strip()[-3:] != 'YES':
+		time.sleep(0.2)
+		lines = temp_raw()
+
+	temp_output = lines[1].find('t=')
+	if temp_output != -1:
+		temp_string = lines[1].strip()[temp_output+2:]
+		temp_c = float(temp_string) / 1000.0
+		return temp_c
+
+while True:
+	print(read_temp())
+	time.sleep(1)
