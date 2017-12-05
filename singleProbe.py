962f7ee02f8c467db4a357db8ed2b3c46dc950ad
dgkirkwood <daniel.g.kirkwood@gmail.com>
2017-12-05 19:16:33 +0000

    change to multiprobe

--- singleProbe.py
+++ singleProbe.py
@@ -1,26 +1,37 @@
 import os
 import time
 
-temp_sensor = '/sys/bus/w1/devices/28-031771c6acff/w1_slave'
+tempSensors = ['28-041781e887ff', '28-041781b282ff', '28-041781a26eff', '28-0317714b31ff', '28-031771e93cff']
 
-def temp_raw():
-	f = open(temp_sensor, 'r')
+temp_output = ''
+
+#temp_sensor = '/sys/bus/w1/devices/28-031771c6acff/w1_slave'
+
+def temp_raw(temp_sensor):
+	sensorAddress = '/sys/bus/w1/devices/'+temp_sensor+'/w1_slave'
+	#print(sensorAddress)
+	f = open(sensorAddress, 'r')
 	lines = f.readlines()
 	f.close
+	#print(lines)	
 	return lines
 
-def read_temp():
-	lines = temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = temp_raw()
+def read_temp(sensor):
 
-	temp_output = lines[1].find('t=')
+
+	lines = temp_raw(sensor)
+	#print(lines)
+	if lines[0].strip()[-3:] == 'YES':
+		#print("YARRRP")
+		time.sleep(0.2)
+		temp_output = lines[1].find('t=')
 	if temp_output != -1:
 		temp_string = lines[1].strip()[temp_output+2:]
 		temp_c = float(temp_string) / 1000.0
 		return temp_c
 
 while True:
-	print(read_temp())
-	time.sleep(1)
+	for sensor in tempSensors:
+		print(sensor)
+		print(read_temp(sensor))
+		time.sleep(1)
